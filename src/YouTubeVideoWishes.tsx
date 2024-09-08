853720569a017199462c2e1dfee4ac2472d06279
MOHIT PAVAN KUMAR GADAMSETTY <kumarmoh@microsoft.com>
2024-09-09 02:13:28 +0530

    youtube link correction

--- src/YouTubeVideoWishes.tsx
+++ src/YouTubeVideoWishes.tsx
@@ -6,7 +6,7 @@ const YouTubeVideo: React.FC = () => {
       <iframe
         width="560"
         height="315"
-        src="https://youtu.be/0_LLhYgwAMo?feature=shared"
+        src="https://www.youtube.com/embed/0_LLhYgwAMo"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
